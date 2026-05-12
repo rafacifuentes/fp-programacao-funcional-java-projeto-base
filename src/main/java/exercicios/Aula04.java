b662d0aa5ac4df6a4f94dd4c83e7f21202d1808b
rafacifuentes <joao.cifuentes@estudante.ifto.edu.br>
2026-05-12 17:17:16 -0300

    resolvidoi

--- src/main/java/exercicios/Aula04.java
+++ src/main/java/exercicios/Aula04.java
@@ -32,12 +32,27 @@ public class Aula04 extends Aula {
         final char homem = 'M';
         final char mulher = 'F';
 
-        System.out.printf("Maior nota de todos os Estudantes: %.2f%n", maiorNotaTodosEstudantes(estudantes.stream()));
-        System.out.printf("Maior nota dos Estudantes homens: %.2f%n", maiorNotaHomens(estudantes.stream()));
-        System.out.printf("Maior nota das mulheres do curso de %s: %.2f%n", curso.getNome(), maiorNotaCursoAndSexo(estudantes.stream(), curso, mulher));
-        System.out.printf("Média de notas dos Estudantes do curso de %s: %.2f%n", curso.getNome(), mediaNotaTodosEstudantesCurso(estudantes.stream(), curso));
-        System.out.printf("Total dos homens do curso de %s: %d%n", curso.getNome(), totalEstudantesCursoAndSexo(estudantes.stream(), curso, homem));
-        System.out.printf("Total das mulheres do curso de %s: %d%n", curso.getNome(), totalEstudantesCursoAndSexo(estudantes.stream(), curso, mulher));
+        System.out.printf("Maior nota de todos os Estudantes: %.2f%n",
+                maiorNotaTodosEstudantes(estudantes.stream()));
+
+        System.out.printf("Maior nota dos Estudantes homens: %.2f%n",
+                maiorNotaHomens(estudantes.stream()));
+
+        System.out.printf("Maior nota das mulheres do curso de %s: %.2f%n",
+                curso.getNome(),
+                maiorNotaCursoAndSexo(estudantes.stream(), curso, mulher));
+
+        System.out.printf("Média de notas dos Estudantes do curso de %s: %.2f%n",
+                curso.getNome(),
+                mediaNotaTodosEstudantesCurso(estudantes.stream(), curso));
+
+        System.out.printf("Total dos homens do curso de %s: %d%n",
+                curso.getNome(),
+                totalEstudantesCursoAndSexo(estudantes.stream(), curso, homem));
+
+        System.out.printf("Total das mulheres do curso de %s: %d%n",
+                curso.getNome(),
+                totalEstudantesCursoAndSexo(estudantes.stream(), curso, mulher));
     }
 
     /**
@@ -47,30 +62,55 @@ public class Aula04 extends Aula {
         new Aula04();
     }
 
-    protected double maiorNotaCursoAndSexo(@NonNull final Stream<Estudante> stream, @NonNull final Curso curso, final char sexo) {
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
+    protected double maiorNotaCursoAndSexo(@NonNull final Stream<Estudante> stream,
+                                           @NonNull final Curso curso,
+                                           final char sexo) {
+
+        return stream
+                .filter(s -> s.getCurso() != null)
+                .filter(s -> s.getCurso().equals(curso))
+                .filter(s -> s.getSexo() == sexo)
+                .mapToDouble(Estudante::getNota)
+                .max()
+                .orElse(0);
     }
 
-    protected long totalEstudantesCursoAndSexo(@NonNull final Stream<Estudante> stream, @NonNull final Curso curso, final char sexo) {
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
+    protected long totalEstudantesCursoAndSexo(@NonNull final Stream<Estudante> stream,
+                                               @NonNull final Curso curso,
+                                               final char sexo) {
+
+        return stream
+                .filter(s -> s.getCurso() != null)
+                .filter(s -> s.getCurso().equals(curso))
+                .filter(s -> s.getSexo() == sexo)
+                .count();
     }
 
-    protected double mediaNotaTodosEstudantesCurso(@NonNull final Stream<Estudante> stream, @NonNull final Curso curso){
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
+    protected double mediaNotaTodosEstudantesCurso(@NonNull final Stream<Estudante> stream,
+                                                   @NonNull final Curso curso){
+
+        return stream
+                .filter(s -> s.getCurso() != null)
+                .filter(s -> s.getCurso().equals(curso))
+                .mapToDouble(Estudante::getNota)
+                .average()
+                .orElse(0);
     }
 
     protected double maiorNotaTodosEstudantes(@NonNull final Stream<Estudante> stream){
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
-    }
 
+        return stream
+                .mapToDouble(Estudante::getNota)
+                .max()
+                .orElse(0);
+    }
 
     protected double maiorNotaHomens(@NonNull final Stream<Estudante> stream){
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
+
+        return stream
+                .filter(s -> s.getSexo() == 'M')
+                .mapToDouble(Estudante::getNota)
+                .max()
+                .orElse(0);
     }
 }
-
